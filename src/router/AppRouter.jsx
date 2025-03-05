5c25539b2f33fc3bcf9ef876f5eb18b5f5550605
fernandovidalpiazza <fervidalpiazza@gmail.com>
2025-03-04 22:55:46 -0300

    rutes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,27 +4,37 @@ import { routes } from "./routes";
 import Login from "../components/pages/login/Login";
 import Register from "../components/pages/register/Register";
 import ForgotPassword from "../components/pages/forgotPassword/ForgotPassword";
-import ProtectedUsers from "./ProtectedUsers";
+import SucursalContainer from "../components/pages/SucursalContainer.jsx/SucursalContainer";
+
 
 const AppRouter = () => {
   return (
     <Routes>
-      {/* Rutas protegidas para usuarios autenticados */}
-      <Route element={<ProtectedUsers />}>
-        <Route path="/" element={<Navbar />}>
-          {routes.map(({ id, path, Element }) => (
-            <Route key={id} path={path} element={<Element />} />
-          ))}
-        </Route>
+      <Route element={<Navbar />}>
+        {routes.map(({ id, path, Element }) => (
+          <Route key={id} path={path} element={<Element />} />
+          
+        ))}
       </Route>
 
-      {/* Rutas públicas */}
+      {/* Login */}
       <Route path="/login" element={<Login />} />
+
+      {/* register  */}
       <Route path="/register" element={<Register />} />
+      
+      <Route path="/sucursales" component={SucursalContainer}/>
+
+      {/* forgot password  */}
       <Route path="/forgot-password" element={<ForgotPassword />} />
 
-      {/* Página 404 */}
-      <Route path="*" element={<h1>Not Found</h1>} />
+     
+
+
+      {/* Not found */}
+      <Route path="*" element={<h1>Not found</h1>} />
+
+
     </Routes>
   );
 };
